9216a591fcc247812c0f752ee7ebb6dd9e803f51
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2023-05-24 13:49:49 -0500

    Rendered Questions/Answers
    added functionality to render questions and answers. Now I need to randomly insert correct answer in the pool of answers.

--- quizzical/src/App.jsx
+++ quizzical/src/App.jsx
@@ -3,22 +3,36 @@ import './App.css'
 import Start from './Start'
 import Quiz from './Quiz'
 import React, { useState, useEffect } from 'react'
+import { decode } from 'html-entities'
 
 function App() {
   const [start, setStart] = useState(false)
   const [questions, setQuestions] = useState([])
 
   useEffect(() => {
-    fetch("https://opentdb.com/api.php?amount=10")
+    fetch("https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiplhttps://opentdb.com/api.php?amount=10&category=15&difficulty=easy&type=multiple")
       .then(res => res.json())
-      .then(data => setQuestions(data))
+      .then(data => setQuestions(data.results))
   }, [])
 
   function toggleStart() {
     setStart(prevStart => !prevStart)
-    console.log(start)
-    console.log(questions.results)
+    console.log(questions)
   }
+  
+  const questionText =
+    questions.map((question, index) => {
+      return (
+        <Quiz
+          key={index}
+          questionText={decode(question.question)}
+          correctAnswer={decode(question.correct_answer)}
+          incorrectAnswer1={decode(question.incorrect_answers[0])}
+          incorrectAnswer2={decode(question.incorrect_answers[1])}
+          incorrectAnswer3={decode(question.incorrect_answers[2])}
+        />
+      )
+    })
 
   return (
     <div>
@@ -28,9 +42,7 @@ function App() {
       />
       }
       {start &&
-      <Quiz
-        
-      />
+      questionText
       }
     </div>
   )
--- quizzical/src/Quiz.jsx
+++ quizzical/src/Quiz.jsx
@@ -1,8 +1,14 @@
+
 export default function Quiz(props) {
     return (
         <div>
-            <h1>Quiz</h1>
-            <button></button>
+            <h3>{props.questionText}</h3>
+            <div>
+                <button>{props.correctAnswer}</button>
+                <button>{props.incorrectAnswer1}</button>
+                <button>{props.incorrectAnswer2}</button>
+                <button>{props.incorrectAnswer3}</button>
+            </div>
         </div>
     )
 }
