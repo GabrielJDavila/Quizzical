ece2ba41174cb5fd50ebdca34ea7c9e69c7bac57
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2023-06-10 20:13:22 -0500

    updates to styles

--- quizzical/src/Start.jsx
+++ quizzical/src/Start.jsx
@@ -3,7 +3,7 @@ export default function Start(props) {
     return (
         <div>
             <h1 className="quiz-title">Quizzical</h1>
-            <p className="quiz-description">The ultimate quiz of the ages</p>
+            <p className="quiz-description">A short quiz to test your video game knowledge</p>
             <button onClick={props.startQuiz} className="start">Start quiz</button>
         </div>
     )
