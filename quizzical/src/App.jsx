e2ea90ece391c8a12297ed84b4cb763b85f3c1c3
GabrielJDavila <107373187+GabrielJDavila@users.noreply.github.com>
2023-08-06 15:11:26 -0500

    Updated file paths for images

--- quizzical/src/App.jsx
+++ quizzical/src/App.jsx
@@ -15,14 +15,6 @@ function App() {
   const [checked, setChecked] = React.useState(false)
   const [score, setScore] = useState(0)
   
-  // function genNewBtn(isCorrect, answer) {
-  //   return {
-  //     answer: answer,
-  //     isCorrect: isCorrect,
-  //     isSelected: false,
-  //     id: nanoid()
-  //   }
-  // }
   useEffect(() => {
     setLoading(true)
     fetch("https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiplhttps://opentdb.com/api.php?amount=10&category=15&difficulty=easy&type=multiple")
@@ -30,23 +22,9 @@ function App() {
       .then(data => {
         setLoading(false)
         const modifiedData = data.results.map(question => {
-          // const quizQuestions = decode(question.question)
           const correctAns = question.correct_answer
-          // const incorrectAns = question.incorrect_answers
-          // let answersArr = []
-          // incorrectAns.map(answer => {
-          //   return answersArr.push(genNewBtn(false, decode(incorrectAns)))
-          // })
-          // const randomNum = Math.floor(Math.random() * 4)
-          // answersArr.splice(randomNum, 0, correctAns)
-          // return {
-          //   id: nanoid(),
-          //   question: quizQuestions,
-          //   answers: answersArr,
-          // }
           const incorrectAnswers = [...question.incorrect_answers, question.correct_answer]
           const shuffledAnswers = shuffleArray(incorrectAnswers)
-          
           const answers = shuffledAnswers.map(answer => ({
             answerText: decode(answer),
             isSelected: false,
@@ -98,23 +76,6 @@ function App() {
         }
       }))
     }
-    // setQuestions(oldQuestions => oldQuestions.map(question => {
-    //   if(question.id === questionId) {
-    //     return {
-    //       ...question,
-    //       answers: question.answers.map(answer => {
-    //         if(answer.id === answerId) {
-    //           console.log(`selected ${answerId}`)
-    //           return {...answer, isSelected: true}
-    //         } else {
-    //           return {...answer, isSelected: false}
-    //         }
-    //       })
-    //     }
-    //   } else {
-    //     return question
-    //   }
-    // }))
   }
 
   function checkAnswers() {
@@ -129,126 +90,16 @@ function App() {
   function reload() {
     window.location.reload()
   }
-  // function selectAnswer(e, answerId) {
-  //   setQuestions(oldQuestions => {
-  //     let newScore = score; // Initialize the new score
-
-  //     const newQuestions = oldQuestions.map(question => {
-  //       if (question.id !== e.target.dataset.questionId) {
-  //         // Preserve the isSelected state for other questions
-  //         return question;
-  //       }
-
-  //       // Update the selected answer for the current question
-        
-  //       const updatedAnswers = question.answers.map(answer => {
-  //           if (answer.id === answerId) {
-  //             // Toggle the isSelected state for the selected answer
-  //             let isSelected = !answer.isSelected
-
-  //             // Update the score if the selected answer is correct
-  //             if (isSelected && answer.isCorrect) {
-  //               newScore++
-  //             } else if (!isSelected && answer.isCorrect) {
-  //               newScore--
-  //             }
-
-  //             return {...answer, isSelected}
-  //           } else {
-  //             return {...answer, isSelected: false}
-  //           }
-              
-  //       })
-        
-  //     })
-
-  //     setScore(newScore); // Update the score state
-  //     return newQuestions; // Return the updated questions
-  //   });
-  // }
-
-  // function selectAnswer(e, answerId) {
-  //   if(e.target.id === answerId) {
-  //     setQuestions(oldQuestions => {
-  //       return oldQuestions.map(question => {
-  //         if(question.id !== e.target.dataset.questionId) {
-  //           return question
-  //         }
-  //         return {
-  //           ...question,
-  //           answers: question.answers.map(answer => {
-  //             if(answer.id === answerId) {
-  //               const isSelected = !answer.isSelected
-  //               if(isSelected)
-  //               setScore(prevScore => prevScore + 1)
-  //               return {...answer, isSelected: !answer.isSelected}
-  //             } else if(answer.isSelected) {
-  //               return {...answer, isSelected: false}
-  //             } else {
-  //               return answer
-  //             }
-  //           })
-  //         }
-  //       })
-  //     })
-  //   }
-    // setQuestions(oldQuestions => {
-    //   return oldQuestions.map(question => {
-    //     console.log(questionId)
-    //     if(questions.id === questionId) {
-    //       console.log("qorks")
-    //       return {
-    //         ...question,
-    //         answers: question.answers.map( answer => {
-    //           console.log(answer.id)
-    //           if(answer.id === answerId) {
-    //             console.log("selected")
-    //             // return {...answer, isSelected: true}
-    //           } else {
-    //             return {...answer, isSelected: false}
-    //           }
-    //         })
-    //       }
-    //     } else {
-    //       return question
-    //     }
-    //   })
-    // })
-    // questions.map(question => {
-    //   if(question.correct_answer === e.target.textContent) {
-    //     question.answers.map(answer => {
-    //       if(answer.isCorrect) {
-    //         re
-    //       }
-    //     })
-    //   }
-    // })
-    
-    
   
-
-  // const questionText = questions.map((question) => {
-  //   return (
-  //     <Quiz
-  //       selectAnswer={selectAnswer}
-  //       key={question.id}
-  //       questionText={decode(question.question)}
-  //       quizQuestions={questions}
-  //       answers={question.answers}
-  //       questionId={question.id}
-  //     />
-  //   )
-  // })
-
   return (
     <div>
       <img
-        src="./src/assets/blob1.png"
+        src="/blob1.png"
         alt="light blue blob"
         className="blue-blob"
       ></img>
       <img
-        src="./src/assets/blob2.png"
+        src="/blob2.png"
         alt="yellow blob"
         className="yellow-blob"
       ></img>
--- quizzical/src/Quiz.jsx
+++ quizzical/src/Quiz.jsx
@@ -20,8 +20,6 @@ export default function Quiz(props) {
                         scale: ".95",
                         fontWeight: "bold"
                     }
-                // } else if(props.checked && answer.isSelected && answer.isCorrect){
-                //     return {backgroundColor: "#94D7A2"}
                 } else if(props.checked && !answer.isSelected && answer.isCorrect) {
                     return {backgroundColor: "#F8BCBC", fontWeight: "bold"}
                 }
@@ -52,77 +50,5 @@ export default function Quiz(props) {
             {questions}
         </div>
     )
-    // function styles(answer) {
-    //     if(answer.isSelected) {
-    //         return {backgroundColor: "#94D7A2"}
-    //     } else if(!answer.isSelected && answer.isCorrect) {
-    //         return {backgroundColor: "#F8BCBC"}
-    //     }
-    // }
-
-    // const questions = props.questions.map(question => {
-    //     const displayQuestion =
-    //         <h3
-    //             key={question.id}
-    //             className=""
-    //         >
-    //             {decode(question.question)}
-    //         </h3>
-
-    //     const displayAnswers = question.answers.map(answer => {
-    //         return (
-    //             <button
-    //                 key={answer.id}
-    //                 className="answer"
-    //                 // style={styles(answer)}
-    //                 onClick={() => props.selectAnswer(question.id, answer.id)}
-    //             >
-    //                 {answer.answerText}
-    //             </button>
-    //         )
-    //     })
-    //     return (
-    //         <div key={nanoid()} className="questions-container">
-    //             {displayQuestion}
-    //             <div key={nanoid()} className="buttons-container">
-    //                 {displayAnswers}
-    //             </div>
-    //         </div>
-    //     )
-    // })
-
-    // const questionId = props.quizQuestions.map(question => {
-    //     return question.id
-    // })
-    
-    // return (
-    //     // <div>
-    //     //     {questions}
-    //     // </div>
-    //     <div className="questions-container">
-    //         <h3>{props.questionText}</h3>
-    //         <div className="buttons-container">
-    //             {props.answers.map(answer => {
-    //                 const styles = {
-    //                     backgroundColor: answer.isSelected ? "blue" : ""
-    //                 }
-    //                 return (
-    //                     <button
-    //                         key={answer.id}
-    //                         className="question"
-    //                         id={answer.id}
-    //                         data-question-id={props.questionId}
-    //                         style={styles}
-    //                         onClick={(e) => props.selectAnswer(e, answer.id)}
-    //                         disabled={answer.isSelected}
-    //                     >
-    //                         {answer.answerText}
-    //                     </button>
-    //                 )
-    //             })}
-                
-    //         </div>
-    //     </div>
-    // )
 }
 
